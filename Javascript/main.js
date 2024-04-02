340243e76bcc56032f4396bb31e4f4ae9b34e48b
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-04-01 19:58:57 -0500

    working on mobile response

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,4 @@
+
 $('.ui.sidebar').sidebar({
     dimPage: false,
     onVisible: function() {
@@ -17,6 +18,64 @@ $('#toggleSidebar').on('click', function() {
 
 
 $(document).ready(function() {
+
+    let navbarElements = $('.ui.menu').find('*');
+    let navbarImageWrap = $('#navbar-image-container').find('*');
+
+        function adjustStyles() {
+            if (window.matchMedia("(min-width: 300px) and (max-width: 601px)").matches) {
+                $('.ui.three.cards .card').css({
+                    'display': 'flex',
+                    'flex-direction': 'column',
+                    'width': '100%'
+                });
+    
+                $('#media_container_id').css({
+                    'width': '100%'
+                });
+
+                $(navbarElements).css({
+                    'padding': '0'
+                })
+
+                $(navbarImageWrap).hide();
+
+                $('#spacer').hide();
+            } else {
+                // Reset the styles if screen width is not between 300px and 601px
+                $('.ui.three.cards .card').css({
+                    'display': '',
+                    'flex-direction': '',
+                    'width': ''
+                });
+    
+                $('.ui.container').css({
+                    'width': ''
+                });
+
+                $(navbarElements).css({
+                    'padding': ''
+                })
+
+                $(navbarImageWrap).show();
+
+                $('#spacer').show();
+            }
+
+            if (window.matchMedia("(max-width: 765px)").matches) {
+                $('.ui.container').addClass('custom-width');
+            } else {
+                $('.ui.container').removeClass('custom-width');
+            }
+        }
+    
+        // Adjust the styles when the page loads
+        adjustStyles();
+    
+        // Adjust the styles when the window is resized
+        $(window).resize(adjustStyles);
+
+
     $.getJSON('../users.json', function(users) {
         var $tableBody = $('.ui.table tbody');
         $.each(users, function(i, user) {
